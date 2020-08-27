e816e58a123ba45ea5a662e00a54bfb4c5106648
yael nirinasoa <yaelnirinasoa@gmail.com>
2020-08-27 23:28:01 +0200

    upload

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Methods: PUT, GET, POST");
 
 $response = array();
 $upload_dir = 'uploads/';
-$server_url = 'http://localhost/PhpAPI';
+$server_url = 'http://phpapiserver.herokuapp.com';
 
 if($_FILES['avatar'])
 {
